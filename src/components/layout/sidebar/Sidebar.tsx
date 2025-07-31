f3cf82a6c33fe4bf4d36be6f15b583e1ff6ed983
joaquin <joaquin.cleva@gmail.com>
2025-07-31 16:33:56 -0300

    feat: modificaciones navbar y sidebar

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,8 +1,43 @@
 import { sidebarLinks } from "../../../common/definitions/constants";
-import { LinkItem } from "../../shared/LinkItem";
 import logo from "../../../assets/img/isologo-ding-degraded.png"
+import { useEffect, useState } from "react";
+import { useAppDispatch } from "../../../redux/store";
+import { toggleSidebar } from "../../../redux/slices/uiSlice";
 
 export const Sidebar = () => {
+
+    const [isTop, setIsTop] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsTop(window.scrollY === 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const dispatch = useAppDispatch();
+    
+    const handleSmoothScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, href: string) => {
+        e.preventDefault();
+        dispatch(toggleSidebar())
+        const targetElement = document.querySelector(href);
+
+        if (targetElement) {
+            const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+            const offset = 7 * 16;
+            const finalPosition = elementPosition - offset;
+
+            window.scrollTo({
+                top: finalPosition,
+                behavior: "smooth",
+            });
+        }
+    };
 
     return (
         <div className={`absolute z-20 h-full md:relative left-0 top-0 flex md:hidden flex-col justify-between bg-white shadow-md transition-all w-64 min-w-64`}>
@@ -13,7 +48,7 @@ export const Sidebar = () => {
                     className="font-bold flex items-center justify-center">
                     <img src={logo} alt="Logo Ding" className='h-8' />
                     <p
-                        className="ml-2 font-bold text-xl flex"
+                        className={`ml-2 font-bold text-xl flex ${isTop && "text-black"}`}
                     >
                         Ding
                     </p>
@@ -22,15 +57,16 @@ export const Sidebar = () => {
                     {
                         sidebarLinks.map((link) => (
                             <li key={link.href}>
-                                <LinkItem
-                                    to={link.href}
-                                    childs={link.childs}
-                                    icon={link.icon}
-                                    variant="sidebar"
-                                    className={`transition-all h-9 [&_span]:justify-start [&_span]:animate-slideRight hover:bg-[#e9e9f5] hover:text-[#18181a]`}
+                                <a
+                                    href={link.href}
+                                    onClick={(e) => handleSmoothScroll(e, link.href)}
+                                    className={`block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 transition-all h-9 [&_span]:justify-start [&_span]:animate-slideRight hover:bg-[#e9e9f5] hover:text-[#18181a]`}
                                 >
-                                    {link.label}
-                                </LinkItem>
+                                <span className="flex items-center gap-2">
+                                    {link.icon} <span className="text-nowrap">{link.label}</span>
+                                </span>
+                                    
+                                </a>
                             </li>
                         ))
                     }
